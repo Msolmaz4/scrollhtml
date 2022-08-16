688ab9a5919172627b22eebd0555c8176fff7ded
kyk <86296198+Msolmaz4@users.noreply.github.com>
2022-08-16 23:47:55 +0200

    scrooll

---
+++ app.js
@@ -0,0 +1,12 @@
+const sr = ScrollReveal()
+
+
+sr.reveal('p',{
+    //bu resimi yavas yavas getiriyor
+    duration:2000,
+    //buda yukaridan getirir
+    origin:'top',
+    mobile:true,
+    //bu syfayi asagi indirki ve yukaru tekra gekdinmiyde yeniden yap,ak ucun
+    reset:true
+})
